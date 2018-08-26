a827a8eee5a8b0c59811b46d03322255dc7d3504
Evan43789596 <10856214@163.com>
2018-08-26 22:05:04 +0800

    feat:初始化mongo和mybatis脚手架

---
+++ springboot-archetype-mybatis/src/main/java/com/eshare/SprintbootArchetypeMybatisApplication.java
@@ -0,0 +1,12 @@
+package com.eshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootArchetypeMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintbootArchetypeMybatisApplication.class, args);
+    }
+}
