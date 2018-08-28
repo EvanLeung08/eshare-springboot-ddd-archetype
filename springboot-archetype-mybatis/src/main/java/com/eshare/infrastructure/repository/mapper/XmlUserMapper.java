8273e3bcff71c50e8e01f016fc342f8403eb9d61
Evan43789596 <10856214@163.com>
2018-08-28 23:32:25 +0800

    feat:初始化hibernate脚手架

--- springboot-archetype-mybatis/src/main/java/com/eshare/infrastructure/repository/mapper/XmlUserMapper.java
+++ springboot-archetype-mybatis/src/main/java/com/eshare/infrastructure/repository/mapper/XmlUserMapper.java
@@ -1,13 +1,5 @@
 package com.eshare.infrastructure.repository.mapper;
 
-import com.eshare.domain.model.user.User;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
-
-import java.util.List;
-
 /**
  * Created by liangyh on 2018/8/28.
  * Email:10856214@163.com
